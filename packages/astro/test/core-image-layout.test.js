2415d990801aeb85e0f7cf6a3cfb29990b302e9e
ematipico <estoppa@cloudflare.com>
2026-02-04 15:34:19 +0000

    feat(csp): support responsive images

--- packages/astro/test/core-image-layout.test.js
+++ packages/astro/test/core-image-layout.test.js
@@ -37,6 +37,7 @@ describe('astro:image:layout', () => {
 			before(async () => {
 				let res = await fixture.fetch('/');
 				let html = await res.text();
+				console.log(html);
 				$ = cheerio.load(html);
 			});
 
@@ -118,11 +119,6 @@ describe('astro:image:layout', () => {
 			it('passes in a parent style as an object', () => {
 				let $img = $('#local-style-object img');
 				assert.match($img.attr('style'), /border:2px red solid/);
-			});
-
-			it('injects a style tag', () => {
-				const style = $('style').text();
-				assert.match(style, /\[data-astro-image\]/);
 			});
 		});
 
@@ -387,11 +383,16 @@ describe('astro:image:layout', () => {
 					assert.ok($picture.attr('class').includes('picture-comp'));
 				});
 
-				it('adds inline style attributes', () => {
+				it('adds data attributes instead of inline styles', () => {
 					let $img = $('#picture-attributes img');
+					// Should have data attributes for CSP compliance
+					assert.ok($img.attr('data-astro-image'));
+					// Should NOT have inline style CSS variables
 					const style = $img.attr('style');
-					assert.match(style, /--fit:/);
-					assert.match(style, /--pos:/);
+					if (style) {
+						assert.ok(!style.includes('--fit:'));
+						assert.ok(!style.includes('--pos:'));
+					}
 				});
 
 				it('passing in style as an object', () => {
